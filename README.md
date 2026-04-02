696477b08acc76fe57e3dd4d7f824a43c9c137f5
iamyuvraj <mail.yuvrajsingh23@gmail.com>
2026-04-02 14:37:02 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Generic PMIS Front End
+
+Versions -
+React: 19.2.4
+Vite: 8.0.1
+Tailwind CSS: 4.2.2
+
+npm -v: 11.12.1
+npx -v: 11.12.1
+node -v: v24.14.1
+nvm -v: 1.2.2
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return <div className="flex justify-center items-center h-screen">
+    <span className="text-3xl font-bold underline">React (JSX) + Vite + Tailwind CSS</span>
+  </div>;
+}
+
+export default App;
